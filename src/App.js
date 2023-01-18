aa897917ba973334c996ecd7c793a0b08b470997
D Roemer <dominicroemer@gmail.com>
2023-01-18 15:50:16 -0800

    Set App.js initial layout and footer placement, ready for newsHomepage component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <div id='page-container'>
+        <div id='content-wrap'>
+          <h1 className="sr-only">Interactive Rating Component</h1>
+
+          <div>
+            And the content goes here.
+          </div>
+
+          <footer className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/dianaroemer">Diana Roemer</a>.
+        </footer>
+
+
+        </div>
+      </div>
+    </main>
+
+
   );
 }
 
